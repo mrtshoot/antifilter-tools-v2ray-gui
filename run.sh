66b1da3dfc88c6d9f60305760df3c42e9a517a75
Hossein Ghorbani <hosi.ghorbani@gmail.com>
2023-03-25 12:26:35 +0330

    add run.sh

---
+++ run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -eu
+
+# Get insite and outside informations
+read -p "Please enter your email address to manage SSL(example@test.com): " email
+read -p "Please enter your Server IP(X.X.X.X): " SERVER_IP
+read -p "Please enter your SSH Password: " SERVER_PASSWORD
+read -p "Please enter your basedomain(EXAMPLE.COM): " domain
+read -p "Please enter your subdomain(test): " subdomain
+read -p "Please enter your ip_address(test): " subdomain
+read -p "Please enter CloudFlate ApiToken: " api_token
+read -p "Would you like to your CloudFlare A Record Proxied or not(y/n)" proxied
+read -p "Enter your web gui v2ray admin username: " config_account
+read -p "Enter your web gui v2ray admin password: " config_password
+read -p "Enter your web gui v2ray admin panel port: " config_port
+
+# Install Requirements
+sudo apt update -y
+sudo apt install -y ansible sshpass
+
+# Create function executer
+func_exec_script() {
+    ansible-playbook -i inventory playbook.yaml --extra-vars="\
+    api_token='${api_token}'  \
+    domain='${domain}' \
+    subdomain='${subdomain}' \
+    ip_address='${SERVER_IP}' \
+    proxied='${proxied}' \
+    config_account='${config_account}' \
+    config_password='${config_password}' \
+    config_port='${config_port}' \
+    email='${email}'"
+}
+
+func_exec_script "$api_token $domain $subdomain $SERVER_IP $proxied $config_account $config_password $config_port $email"
